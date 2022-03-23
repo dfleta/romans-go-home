cd502a7c51899e077b9262750460c1f9e7e73e33
dfleta <gelpiorama@gmail.com>
2022-03-23 20:25:18 +0100

    docs(README): Incluir enunciado ejercicio y recursos

--- src/test/java/org/foobarspam/kataromannumerals/Test/NumeroRomanoTest.java
+++ src/test/java/org/foobarspam/kataromannumerals/Test/NumeroRomanoTest.java
@@ -43,7 +43,7 @@ public class NumeroRomanoTest {
 
         /**
          * El caso MMMM es control de errores 
-         * y no puede estas en el test de la logica
+         * y no puede estar en el test de la logica
          * Asumimos que la entrada es correcta.
          * Sino, hay que programar la gestion de errores
          */
